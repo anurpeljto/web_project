62a22c5eb3d37838cf62340dd7057fa1e3c993fd
anurpeljto <anurpeljto@stu.ibu.edu.ba>
2024-03-22 11:48:15 +0100

    Modify navbar links
    Change from links to navlinks and add function to adjust color accordingly

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,38 @@
 import React from 'react';
 import logo from '../assets/images/logo.jpg';
 import {NavLink} from 'react-router-dom';
-import {Link} from 'react-router-dom';
 
 const Navbar = () => {
+
+    const links = ({isActive}) => isActive ? 'text-white bg-amber-700 hover:bg-amber-900 hover:text-white rounded-md px-3 py-2' : 'text-white hover:bg-amber-900 hover:text-white rounded-md px-3 py-2'
+
   return (
     <nav className="bg-green-700 border-b border-green-500">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="flex h-20 items-center justift-between">
                 <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
-                    <Link className="flex flex-shrink-0 items-center mr-4"
+                    <NavLink className="flex flex-shrink-0 items-center mr-4"
                     to=''>
                         <img className='h-10 w-auto' src={logo} alt='ToDo List' />
                         <span className="hidden md:block text-white text-2xl font-bold ml-2">
                             ToDo List
                         </span>
-                    </Link>
+                    </NavLink>
 
                     <div className="md:ml-auto">
                         <div className="flex space-x-2">
 
-                            <Link to='/'
-                            className="text-white bg-amber-700 hover:bg-amber-900 hover:text-white rounded-md px-3 py-2">
+                            <NavLink to='/'
+                            className={links}>
                                 Home
-                            </Link>
+                            </NavLink>
 
-                            <Link to='/tasks/'
-                            className='text-white hover:bg-amber-900 hover:text-white rounded-md px-3 py-2'>
+                            <NavLink to='/tasks/'
+                            className={links}>
                                 Tasks
-                            </Link>
+                            </NavLink>
 
-                            <Link to='/view-task' className='text-white hover:bg-amber-900 hover:text-white rounded-md px-3 py-2'>Add item</Link>
+                            <NavLink to='/view-task' className={links}>Add item</NavLink>
 
                         </div>
                     </div>
