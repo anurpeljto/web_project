63ff23f6e87ece0a2043516cf697f6c3e2030b7b
anurpeljto <anurpeljto@stu.ibu.edu.ba>
2024-03-22 11:11:31 +0100

    Create individual item component

---
+++ src/components/Item.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Item = ({item}) => {
+  return (
+    <div className="bg-white rounded-xl shadow-md relative">
+                        <div className="p-4">
+                            <div className="mb-6">
+                                <div className="text-gray-600 my-2">{item.duration}</div>
+                                <h3 className="text-xl font-bold">{item.title}</h3>
+                            </div>
+
+                            <div className="mb-5">
+                                {item.description}
+                            </div>
+
+                            <div className="text-green-500">Due {item.due_date}</div>
+                            <div className="border border-gray-100 mb-5"></div>
+                            <div className="flex flex-col lg:flex-row justify-between mb-4">
+                                <div className="text-amber-700 mb-3">
+                                    {item.category}
+                                </div>
+                                <a href="chore.html" className='h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm'>
+                                    Modify chore
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+  )
+}
+
+export default Item
--- src/components/Upcoming.jsx
+++ src/components/Upcoming.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import todo from '../todo.json';
+import Item from './Item';
 
 const Upcoming = () => {
   return (
@@ -11,29 +12,7 @@ const Upcoming = () => {
                 </h2>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                     {todo.map((item) => (
-                        <div className="bg-white rounded-xl shadow-md relative">
-                        <div className="p-4">
-                            <div className="mb-6">
-                                <div className="text-gray-600 my-2">{item.duration}</div>
-                                <h3 className="text-xl font-bold">{item.title}</h3>
-                            </div>
-
-                            <div className="mb-5">
-                                {item.description}
-                            </div>
-
-                            <div className="text-green-500">{item.due_date}</div>
-                            <div className="border border-gray-100 mb-5"></div>
-                            <div className="flex flex-col lg:flex-row justify-between mb-4">
-                                <div className="text-amber-700 mb-3">
-                                    {item.category}
-                                </div>
-                                <a href="chore.html" className='h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm'>
-                                    Modify chore
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+                        <Item item={item} key = {item.id}/>
                     ))}
                     
                 </div>
