94b75c1248ee265e4be19a29b64276647ad654a8
anurpeljto <anurpeljto@stu.ibu.edu.ba>
2024-03-22 11:08:08 +0100

    Create Upcoming.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom';
+import Upcoming from './components/Upcoming';
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <span>test</span>
+      <Upcoming />
     </>
   )
 }
