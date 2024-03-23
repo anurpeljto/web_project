2c0433ca1c01e68b7b6057d1e3632f91d93b4457
anurpeljto <anurpeljto@stu.ibu.edu.ba>
2024-03-23 09:42:59 +0100

    Add 'go back' feature to TaskPage.jsx

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -2,12 +2,24 @@ import React from 'react'
 import {useState, useEffect} from 'react';
 import todo from '../todo.json';
 import {useLoaderData, useParams} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import {FaArrowLeft} from 'react-icons/fa';
 
 const TaskPage = () => {
   const {id} = useParams();
   const task = useLoaderData();
   return (
-    <h1>{task.title}</h1>
+    // making a "go back" feature
+    <section>
+      <div className="container m-auto py-6 px-6">
+        <Link to='/tasks' className='text-green-500 hover:text-green-600 flex items-center'>
+          <FaArrowLeft className='mr-2' />
+          Go Back
+        </Link>
+      </div>
+    </section>
+
+    
   )
 };
 
