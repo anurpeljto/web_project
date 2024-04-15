bda2dc4c6f9668a98c7f6b8440f58115b15afefd
anurpeljto <anurpeljto@stu.ibu.edu.ba>
2024-04-15 21:26:13 +0200

    Load all tasks from DB

---
+++ api/get_upcoming_tasks.php
@@ -0,0 +1,22 @@
+<?php 
+
+$host = '127.0.0.1:8889';
+$db = 'my_database';
+$user = 'root';
+$password = 'root';
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $password);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare('SELECT * FROM tasks; LIMIT 3');
+    $stmt->execute();
+
+    $tasks = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode(['tasks'=>$tasks]);
+} catch (PDOException $e){
+    echo 'Failed to connect to DB, ' . $e;
+}
+finally {
+    $conn = null;
+}
