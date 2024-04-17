6beb545440f89b78282dd19af11d05ad4b318950
anurpeljto <anurpeljto@stu.ibu.edu.ba>
2024-04-17 10:04:16 +0200

    Add dao class to call and connect to DB

--- api/get_upcoming_tasks.php
+++ api/get_upcoming_tasks.php
@@ -1,21 +1,17 @@
 <?php 
 
-$host = '127.0.0.1:8889';
-$db = 'my_database';
-$user = 'root';
-$password = 'root';
+require_once 'DB_connector.php';
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $password);
-
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare('SELECT * FROM tasks ORDER BY due_date LIMIT 5;');
+    $database = new Connector();
+    $conn = $database->connect();
+    $stmt = $conn->prepare('SELECT * FROM tasks WHERE due_date > CURDATE() ORDER BY due_date LIMIT 5;');
     $stmt->execute();
 
     $tasks = $stmt->fetchAll(PDO::FETCH_ASSOC);
     echo json_encode(['tasks'=>$tasks]);
 } catch (PDOException $e){
-    echo 'Failed to connect to DB, ' . $e;
+    echo 'Failed to connect to DB, ' . $e->getMessage();
 }
 finally {
     $conn = null;
