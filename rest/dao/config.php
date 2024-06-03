5bd90272e94c4abf46bb3e06bf31eeb4ef7aef25
anurpeljto <anurpeljto@stu.ibu.edu.ba>
2024-06-03 10:46:39 +0200

    Update db

--- rest/dao/config.php
+++ rest/dao/config.php
@@ -1,8 +1,8 @@
 <?php
-define('host', '127.0.0.1:8889');
-define('db', 'my_database');
-define('user', 'root');
-define('password', 'root');
+define('host', 'db-mysql-fra1-59703-do-user-16822868-0.c.db.ondigitalocean.com:25060');
+define('db', 'defaultdb');
+define('user', 'doadmin');
+define('password', 'AVNS_TNz0TF9D9ujiiyMUqSu');
 //private $host = '127.0.0.1:8889';
 // private $db = 'my_database';
 // private $user = 'root';
